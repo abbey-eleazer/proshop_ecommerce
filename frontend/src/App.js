ce28c654f16c7ec99e600fa7e732d65a6f2715ee
abbey-eleazer <abbeyeleazer@gmail.com>
2024-05-01 11:36:29 +0200

    custom styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 function App() {
   return (
-   <h1>Welcome</h1>
+  <>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <Outlet />
+      </Container>
+    </main>
+    <Footer />
+  </>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/index.css'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
